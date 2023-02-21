eca7239184efc83e309eff14002bda57b6aa4ef9
Jorge Rojas <jorge.otto.415@gmail.com>
2023-02-20 21:51:49 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xV0lk/webdev-go
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const DPORT = 3000
+
+func main() {
+	http.HandleFunc("/", handleFunc)
+	fmt.Printf("Starting server on port: %v", DPORT)
+	http.ListenAndServe(fmt.Sprintf(":%v", DPORT), nil)
+}
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Incoming request")
+	fmt.Fprint(w, "<h1>Test</h1>")
+}
