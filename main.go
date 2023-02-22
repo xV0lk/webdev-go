1dc36a71352d66e0cf890624cec8d07396a0129e
Jorge Rojas <jorge.otto.415@gmail.com>
2023-02-22 04:13:00 -0500

    rewrite handler for a basic switch router

--- main.go
+++ main.go
@@ -7,13 +7,35 @@ import (
 
 const DPORT = 3000
 
-func main() {
-	http.HandleFunc("/", handleFunc)
-	fmt.Printf("Starting server on port: %v", DPORT)
-	http.ListenAndServe(fmt.Sprintf(":%v", DPORT), nil)
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Incoming request")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Test <em>again 2</em></h1><style>body {background-color: #3B3B3B;color: white}</style>")
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Incoming request")
-	fmt.Fprint(w, "<h1>Test <em>again</em></h1>")
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Contact request")
+	fmt.Fprint(w, "<h1>Contact Page</h1><style>body {background-color: #3B3B3B;color: white}</style>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		handleHome(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// the following line wont work because cant display as html, only plain text
+		// http.Error(w, fmt.Sprintf("<h1>Not Found</h1><p>You requested %s</p><style>body {background-color: #3B3B3B;color: white}</style>", r.URL.Path), http.StatusNotFound)
+
+		// i will try writing to the header the status code
+		w.WriteHeader(http.StatusTeapot)
+		fmt.Fprintf(w, "<h1>Path Page</h1><p>You requested %s</p><style>body {background-color: #3B3B3B;color: white}</style>", r.URL.Path)
+	}
+}
+
+func main() {
+	var webRouter http.HandlerFunc = pathHandler
+	fmt.Printf("Starting server on port: %v\n", DPORT)
+	http.ListenAndServe(fmt.Sprintf(":%v", DPORT), webRouter)
 }
