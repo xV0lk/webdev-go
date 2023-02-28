1416d660d582cbd74237f52272ec7d3b4c672f4d
Jorge Rojas <jorge.otto.415@gmail.com>
2023-02-28 07:14:56 -0500

    update template function and add contact and faq

--- handlers.go
+++ handlers.go
@@ -8,47 +8,33 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func ProcessTmpl(n string, w *http.ResponseWriter) error {
+func ProcessTmpl(w http.ResponseWriter, n string) {
+	w.Header().Set("Content-Type", "text/html")
 	tmpl, err := template.ParseFiles(fmt.Sprintf("templates/%s.tmpl", n))
 	if err != nil {
-		return err
+		http.Error(w, fmt.Errorf("server error while parsing file: %w", err).Error(), http.StatusInternalServerError)
+		return
 	}
-	err = tmpl.Execute(*w, nil)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
-		return err
+		http.Error(w, fmt.Errorf("server error while parsing file: %w", err).Error(), http.StatusInternalServerError)
+		return
 	}
-	return nil
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home request")
-	w.Header().Set("Content-Type", "text/html")
-	err := ProcessTmpl("home", &w)
-	if err != nil {
-		http.Error(w, fmt.Errorf("server error while parsing file: %w", err).Error(), http.StatusInternalServerError)
-	}
+	ProcessTmpl(w, "home")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Contact request")
-	fmt.Fprint(w, `<h1>Contact Page</h1
-	><style>body {background-color: #3B3B3B;color: white}</style>`)
+	ProcessTmpl(w, "contact")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Faq Page")
-	fmt.Fprint(w, `<h1>Faq</h1>
-	<ul>
-		<li>
-			<p><strong>This is a question</strong></p>
-			<p>This is the answer</p>
-		</li>
-		<li>
-			<p><strong>This is another question: </strong></p>
-			<p>This is another answer</p>
-		</li>
-	</ul>
-	<style>body {background-color: #3B3B3B;color: white}</style>`)
+	ProcessTmpl(w, "faq")
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
